061c8a54a6bdd4a38275626fbf433301170afedf
Alexander <apvannug.lhl.webdev@gmail.com>
2022-11-14 23:07:35 -0700

    refactor second url id due to error with leading number

--- url_database.js
+++ url_database.js
@@ -1,9 +1,10 @@
 // urlDatabse Storage
 const urlDatabase = {
-  "b2xVn2": {
+  b2xVn2: {
     longURL: "http://www.lighthouselabs.ca",
     userID: "lighthouse-id"
   },
+  
   "9sm5xK": {
     longURL: "http://www.google.com",
     userID: "google-id"
