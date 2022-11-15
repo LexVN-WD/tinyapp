09a29f9f9f5968fe89d1e8c7821f7a87e33ac6ad
Alexander <apvannug.lhl.webdev@gmail.com>
2022-11-14 22:30:19 -0700

    create storage for refactored urlDatabase object

---
+++ url_database.js
@@ -0,0 +1,13 @@
+// urlDatabse Storage
+const urlDatabase = {
+  "b2xVn2": {
+    longURL: "http://www.lighthouselabs.ca",
+    userID: "lighthouse-id"
+  },
+  "9sm5xK": {
+    longURL: "http://www.google.com",
+    userID: "google-id"
+  },
+};
+
+module.exports = urlDatabase;
